f8b1d626fb339aad79400b2f72c89e6b3347481c
vladislav zhukov <vzhukov@protonmail.ch>
2018-12-13 16:30:23 +0500

    Initial Commit

---
+++ VZiOSNetwork/VZiOSNetwork.h
@@ -0,0 +1,19 @@
+//
+//  VZiOSNetwork.h
+//  VZiOSNetwork
+//
+//  Created by Maximal Mac on 13/12/2018.
+//  Copyright © 2018 vc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VZiOSNetwork.
+FOUNDATION_EXPORT double VZiOSNetworkVersionNumber;
+
+//! Project version string for VZiOSNetwork.
+FOUNDATION_EXPORT const unsigned char VZiOSNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VZiOSNetwork/PublicHeader.h>
+
+
